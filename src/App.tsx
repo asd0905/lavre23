01dcddf612454e37f6334b7e8fb69ed7232b91ef
asd0905 <asd0905@naver.com>
2023-05-30 21:35:31 +0900

    test-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import styled from "styled-components";
+
+const SliderBox = styled.div<{ img: string; y?: string }>`
+	background-image: url(${(props) => props.img});
+	min-height: 80vh;
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+	background-position-y: ${(props) => (props.y ? props.y : 0)}px;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const settingsTop = {
+		dots: false,
+		infinite: true,
+		speed: 800,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		// autoplay: true,
+	};
+	const settingsBtm = {
+		...settingsTop,
+		rtl: true,
+	};
+	return (
+		<>
+			<Slider {...settingsTop}>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204950644.jpg`}
+					y={"374"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204957201_02.jpg`}
+					y={"380"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204930616.jpg`}
+					y={"355"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204926853_02.jpg`}
+					y={"397"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204919139_01.jpg`}
+					y={"385"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204904699_01.jpg`}
+					y={"355"}
+				></SliderBox>
+			</Slider>
+			<Slider {...settingsBtm}>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204950644.jpg`}
+					y={"-362"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204957201_02.jpg`}
+					y={"-362"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204930616.jpg`}
+					y={"-388"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204926853_02.jpg`}
+					y={"-343"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204919139_01.jpg`}
+					y={"-366"}
+				></SliderBox>
+				<SliderBox
+					img={`${process.env.PUBLIC_URL}/img/KakaoTalk_20230530_204904699_01.jpg`}
+					y={"-388"}
+				></SliderBox>
+			</Slider>
+		</>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-style: normal;
+  }
+`;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<GlobalStyle />
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
